0edb9e9cd192b11904a31387bfa7244721071a7e
Bobinho100 <114113147+Bobinho100@users.noreply.github.com>
2023-07-12 22:44:15 -0600

    Made routes for the components as well as pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import { Route, Routes, NavLink, Navigate, BrowserRouter as Router} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+
+
+
+        <Routes>
+          <Route path="/" element = {<Home />}></Route>
+          <Route path="/about" element = {<About />}></Route>
+          <Route path="/projects" element = {<Projects />}></Route>
+        </Routes>
+    </Router>
+
+
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import '../styles/Navbar.css'
+import {FaBars}  from 'react-icons/fa'
+import {GrClose} from 'react-icons/gr'
+
+
+const Navbar = () => {
+
+    const [hamburgerOpen, setHamburgerOpen] = useState(false)
+
+    const togglehamburger = () => {
+        setHamburgerOpen(!hamburgerOpen)
+    }
+
+  return (
+    <>
+        <div className='header'>
+            <NavLink to= '/'>
+                <h3>Bernard Website</h3>
+
+
+            </NavLink>
+
+            <button className='hamburger'    onClick={ togglehamburger}  >
+
+                {hamburgerOpen ? <GrClose className='closeMenu' />:
+
+                    <FaBars  className='openMenu'/>
+
+                    }
+
+            </button>
+
+            <nav className={`navigation ${hamburgerOpen ? 'open': 'closed'}`}>
+
+
+
+                <NavLink to = '/about'   onClick={()=> setHamburgerOpen(!hamburgerOpen)}  >About</NavLink>
+                <NavLink to= '/projects'  onClick={()=> setHamburgerOpen(!hamburgerOpen)}    >Projects</NavLink>
+                <NavLink to= '/experience'  onClick={()=> setHamburgerOpen(!hamburgerOpen)}     >Experience</NavLink>
+
+
+
+            </nav>
+
+
+        </div>
+
+
+    </>
+  )
+}
+
+export default Navbar
